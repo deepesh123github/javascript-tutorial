e679aa4a807a4fe257e2ebb054b4a00bbc200be0
deepesh123github <133794619+deepesh123github@users.noreply.github.com>
2023-10-14 06:28:02 -0600

    Add files via upload

---
+++ jsbyhitesh/conversion.js
@@ -0,0 +1,50 @@
+let score =   "deepesh"
+
+console.log(typeof score) ;
+console.log(typeof (score));
+
+let valueInNumber = Number(score) ;
+console.log(typeof valueInNumber) ;
+ console.log( valueInNumber) ; 
+//   input /output
+//  "33" => 33
+// "33abc" => NaN
+//  true => 1;  false=> 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) ;
+//   input /output
+//  1 => true 
+// 0=> false
+//  "" => false  
+// "deepesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber) 
+console.log(stringNumber) ;
+console.log(typeof  stringNumber) ; 
+
+//**************************Operations****************************************
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue) ;
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+
+let str1 = "hello"
+let str2 = "deepesh"
+
+let str3 = str1 + str2 
+console.log(str3) ;
+
+console.log("1" + 2) ;
+console.log(1 + "2") ;
+console.log(1 + "2"+2);
+console.log(2+1 + "2"); 
---
+++ jsbyhitesh/datatype.js
@@ -0,0 +1,23 @@
+"use strict" ; // we write  strict because we use newer version of javascript 
+
+//code readability should be high 
+let name = "deepesh" 
+let age = 18
+let isLoggedIn = false
+let state ;
+// number => 2 to power 53
+// bigint 
+//  string => ""
+// boolean => true/false 
+// null => standalone value 
+//  undefined => 
+// symbol  => unique 
+
+//  object 
+
+console.log(typeof "deepesh");
+
+console.log(typeof null);    // type is object  because null is object 
+
+console.log(typeof undefined);   //type is undefined 
+
